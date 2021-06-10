c0994503b38b3c8b1bc4d6fe0e88f1f29ea9c72e
wujiang <1473670836@qq.com>
2021-06-10 17:11:37 +0800

    leetcode上的一些练习

--- algorithm/src/com/java/leetcode/simple/GetIntersectionNode.java
+++ algorithm/src/com/java/leetcode/simple/GetIntersectionNode.java
@@ -11,4 +11,42 @@ public class GetIntersectionNode {
         }
         return null;
     }
+
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+
+        int sizeA = 0, sizeB = 0;
+        ListNode firstA = headA, firstB = headB;
+        //计算出两个链表的长度
+        while (headA != null) {
+            sizeA++;
+            headA = headA.next;
+        }
+        while (headB != null) {
+            sizeB++;
+            headB = headB.next;
+        }
+
+        //将指针移到两者同一起点
+        if (sizeA > sizeB) {
+            for (int i = 0; i < sizeA - sizeB; i++) {
+                firstA = firstA.next;
+            }
+        } else {
+            for (int i = 0; i < sizeB - sizeA; i++) {
+                firstB = firstB.next;
+            }
+        }
+
+        while (firstA!=null&&firstB!=null){
+            if(firstA== firstB){
+                return firstA;
+            }
+            firstA=firstA.next;
+            firstB=firstB.next;
+        }
+
+        return null;
+
+    }
+
 }
